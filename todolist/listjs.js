fff7302e681c5ba46a7f2b3c59e35ebcf4873d65
james philiossaint <yourGitHub@email.comjamesphiliossaint>
2022-05-30 18:16:36 -0400

    first commit i think

--- todolist/listjs.js
+++ todolist/listjs.js
@@ -1,34 +1,31 @@
-document.querySelector(".btn").addEventListener('click', toDos)
-document.querySelector("ul").addEventListener('click', function(e){
-    e.target.classList.toggle('strikeThrough')
-    let num = document.querySelectorAll('li').length
-    let num2 = document.querySelectorAll('.strikethrough').length
-    document.querySelector('.toDos').innerText = num - num2
-})
-
-document.querySelector('.clrList').addEventListener('click', clrTheList)
-document.querySelector('clrComplete').addEventListener('click', clrComplete)
+document.querySelector('.btn').addEventListener('click', toDos)
+let numofToDos = document.querySelector('.toDos')
 
 function toDos(){
-    let input = document.querySelector('.input').value
-
+    let input = document.querySelector('input').value
     const node = document.createElement('li')
     const textNode = document.createTextNode(input)
     node.appendChild(textNode)
     document.getElementById('toDoList').appendChild(node)
-    let num = document.querySelector('li').length
-    document.querySelector('.toDos'.innerText) = num
+    document.querySelector('.input').value = ' '
+    Number(numofToDos.innerText ++)
 }
 
+document.querySelector('ul').addEventListener('click', function(e){
+    e.target.classList.toggle('strikeThrough')
+    Number(numofToDos.innerText --)
+})
 
-function clrTheList(){
+document.querySelector('.clrList').addEventListener('click', clrList)
+function clrList(){
     document.querySelectorAll('li').forEach(li => {
         li.remove()
     })
+    numofToDos.innerText = 0
 }
-
+document.querySelector('.clrDone').addEventListener('click', clrComplete)
 function clrComplete(){
-    document.querySelectorAll('strikeThrough').forEach(li => {
+    document.querySelectorAll('.strikeThrough').forEach(li => {
         li.remove()
     })
 }
